5c1a3aa99fea689737793449e863ce5a210557b4
ivica <blizujemars@gmail.com>
2023-04-30 17:08:53 +0200

    add printReport

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { crawlPage } = require('./crawl.js');
+const { printReport } = require('./report.js');
 
 async function main() {
   if (process.argv.length === 3) {
     console.log(`Crawler starting at base URL ${process.argv[2]}`);
     const pages = await crawlPage(process.argv[2], process.argv[2], {});
-    console.log(pages);
+    printReport(pages);
   } else {
     console.error('Error. Try again');
     return;
