1ea266610d5a533d1df5883a9e54dea86d716309
ivica <blizujemars@gmail.com>
2023-04-30 09:45:31 +0200

    add crawlPage fn

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('heyy');
+const { crawlPage } = require('./crawl.js');
+
+async function main() {
+  if (process.argv.length === 3) {
+    console.log(`Crawler starting at base URL ${process.argv[2]}`);
+    await crawlPage(process.argv[2]);
+  } else {
+    console.error('Error. Try again');
+    return;
+  }
+}
+
+main();
